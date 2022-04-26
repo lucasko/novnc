00752f77a832567b147a31b600ee7c272937e4e4
Lucas Ko <lucasko.tw@gmail.com>
2022-04-26 22:03:19 +0800

    chrome

--- src/centos/install/chrome.sh
+++ src/centos/install/chrome.sh
@@ -8,8 +8,9 @@ yum localinstall -y google-chrome-stable_current_x86_64.rpm
 yum clean all
 
 #ln -s /usr/bin/chrome-browser /usr/bin/google-chrome
+ln -s /usr/bin/google-chrome /usr/bin/chrome-browser
 
 ### fix to start chrome in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
 echo "CHROME_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chrome-browser.init
 # add `source $INST_SCRIPTS/install/chrome-wrapper`string before last line
-#sed -i '$isource $INST_SCRIPTS/chrome-wrapper' /usr/lib64/chrome-browser/chrome-browser.sh
+sed -i '$isource $INST_SCRIPTS/chrome-wrapper' /usr/lib64/chrome-browser/chrome-browser.sh
